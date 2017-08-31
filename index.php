80267a3b988a59b3bc057a79aad3d824bc857f03
Dominic Fajilan <dfajilan@csumb.edu>
2017-08-31 22:15:41 +0000

    Lab 1

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="utf-8" />
+        <title> Dominic Fajilan: Personal Website </title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        <header>
+            <h1> Dominic Fajilan </h1>
+        </header>
+            <nav>
+                <hr width="50%" />
+                <a href="index.php" id="home">Home</a>
+                <a href="about.html">About</a>
+                <a href="contact.html">Contact</a>
+            </nav>
+            
+            <br /><br />
+            
+            <main>
+                
+                <figure id="me">
+                    <img src="img/dominic_fajilan.jpg" alt="Picture of Dominic Fajilan" />
+                </figure>
+                
+                <div id="welcomeText">
+                    Hello! <br />
+                    <p>Thank you for visiting my professional portfolio 
+                    website.</p>
+                    
+                    <p>I am currently a student at CSUMB studying Computer Science <br />
+                    with a concentration in Software Design. </p>
+                    
+                    <p>Feel free to contact me!</p>
+                    
+                    <br /><br />
+                    
+                    <em>"With great power comes great
+                    <strong>responsibility</strong>"</em><br />
+                    - Uncle Ben
+                    
+                </div>
+                
+            </main>
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+            <hr>
+            CST 336. 2017&copy; Fajilan <br />
+            <strong>Disclaimer:</strong> The information in this webpage is 
+            fictitous. <br />
+            
+            It is used for academic purposes only
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
